d2fd1e5f1e5722bfb24395ae97dfdfc5671ff09b
Lovre Mrčela <lovre.mrcela@fer.hr>
2021-03-07 23:12:33 +0100

    promjena.

--- gramatika/primjeri/run_all.sh
+++ gramatika/primjeri/run_all.sh
@@ -2,6 +2,6 @@
 for file in $(ls *.tex); do
   echo -n "Doing $file... "
   xelatex -synctex=1 -interaction=nonstopmode $file > /dev/null
-  convert -density 300 ${file%.*}.pdf ${file%.*}.png > /dev/null
+  convert -density 300 ${file%.*}.pdf png/${file%.*}.png > /dev/null
   echo "done!"
 done
