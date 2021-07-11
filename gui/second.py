03a0ac193015e1a13013eb1e5f297d7d4db4aea0
Lovre Mrčela <lovre.mrcela@fer.hr>
2021-07-11 15:52:47 +0200

    m

--- gui/second.py
+++ gui/second.py
@@ -1,7 +1,5 @@
-
 import tkinter as tk
 from tkinter import ttk
-
 
 class App(tk.Tk):
     def __init__(self):
@@ -14,13 +12,13 @@ class App(tk.Tk):
 
         # label
         label = ttk.Label(self, text='Name:')
-        label.grid(column=0, row=0, padx=10, pady=10,  sticky='w')
+        label.grid(column=0, row=0, padx=10, pady=10, sticky='w')
         # entry
         textbox = ttk.Entry(self)
-        textbox.grid(column=1, row=0, padx=10, pady=10,  sticky='w')
+        textbox.grid(column=1, row=0, padx=10, pady=10, sticky='w')
         # button
         btn = ttk.Button(self, text='Show')
-        btn.grid(column=2, row=0, padx=10, pady=10,  sticky='w')
+        btn.grid(column=2, row=0, padx=10, pady=10, sticky='w')
 
         # radio button
         self.selected_theme = tk.StringVar()
@@ -38,7 +36,6 @@ class App(tk.Tk):
 
     def change_theme(self):
         self.style.theme_use(self.selected_theme.get())
-
 
 if __name__ == "__main__":
     app = App()
